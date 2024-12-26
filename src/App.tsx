a9e2342da76efa64294516d47c9c0ce4036617fc
Collin Berg <berg.collin@gmail.com>
2024-12-25 19:01:41 -0500

    Add AppNav component and integrate boons display with improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 import BuildSelector from "./components/BuildSelector";
+import AppNav from "./components/AppNav";
 import FilterMenu from './components/FilterMenu';
 import Card from './components/Card'
 import Weapons from './components/Weapons'
+
 import {weaponsData} from "./data/Weapons"
+import {attack} from "./data/BoonsAttack"
+import {special} from "./data/BoonsSpecial"
+import {cast} from "./data/BoonsCast"
 
 import './App.css'
 
@@ -31,16 +36,14 @@ function App() {
     cast: ""
   });
   const [menuStatus, setMenuStatus] = useState(false);
-  const [weaponStatus, setWeaponStatus] = useState(false);
-  const [aspectStatus, setAspectStatus] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
   const [aspect, setAspect] = useState(0);
 
 
 
 
   const updateBuild = (item:number) => {
-    setWeaponStatus(false),
-    setAspectStatus(true),
+    setActiveIndex(2),
     setAspect(item),
     setBuild( build => (
       {...build,
@@ -57,8 +60,7 @@ function App() {
 
   const updateAspect = (item:number) => {
     setMenuStatus(true)
-    setWeaponStatus(false),
-    setAspectStatus(false)
+    setActiveIndex(0),
     setBuild( build => (
       {...build,
         aspect: {
@@ -75,56 +77,73 @@ function App() {
   return (
     <>
       <div className="site-wrap">
-        <header className="p-4 sm:ml-64">
-          <div className="header-inner flex justify-center flex-col">
-              {build.cast === "" ? <span className="text-center text-white block py-5 text-2xl">No Boons Added</span> : null}
-          </div>
-        </header>
-        <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-            <div className="h-full px-4 py-4 overflow-y-auto">
-                <section id='weapon-side' className="space-y-2 font-medium">
-                    <h2>Weapon</h2>
-                    <div className='weapon_wrap'>
-                        <BuildSelector onClick={() => setWeaponStatus(true)} attribute={build.weapon}>Weapon</BuildSelector>
-                    </div>
-                    <div className="weapon_wrap">
-                        <BuildSelector onClick={() => setAspectStatus(true)} attribute={build.aspect}>Aspect</BuildSelector>
-                    </div>
-                </section>
-                <section id='aspect-side'>
-                    <h2>Abilities</h2>
-                    <BuildSelector>Attack</BuildSelector>
-                    <BuildSelector>Special</BuildSelector>
-                    <BuildSelector>Cast</BuildSelector>
-                    <BuildSelector>Dash</BuildSelector>
-                    <BuildSelector>Call</BuildSelector>
-                </section>
-            </div>
-        </aside>
-        <main className='p-4 sm:ml-64'>
-            {menuStatus && <FilterMenu />}
+        <AppNav />
+        <div className="flex w-full pt-16 overflow-hidden" aria-hidden="true">
+          <aside id="default-sidebar" className="fixed pt-12 top-0 left-0 z-20 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+              <div className="h-full px-4 py-8 overflow-y-auto">
+                  <section id='weapon-side' className="space-y-2 font-medium">
+                      <h2>Weapon</h2>
+                      <div className='weapon_wrap'>
+                          <BuildSelector onClick={() => setActiveIndex(1)} attribute={build.weapon}>Weapon</BuildSelector>
+                      </div>
+                      <div className="weapon_wrap">
+                          <BuildSelector onClick={() => setActiveIndex(2)} attribute={build.aspect}>Aspect</BuildSelector>
+                      </div>
+                  </section>
+                  <section id='aspect-side'>
+                      <h2>Abilities</h2>
+                      <BuildSelector onClick={() => setActiveIndex(3)}>Attack</BuildSelector>
+                      <BuildSelector onClick={() => setActiveIndex(4)}>Special</BuildSelector>
+                      <BuildSelector onClick={() => setActiveIndex(5)}>Cast</BuildSelector>
+                      <BuildSelector>Dash</BuildSelector>
+                      <BuildSelector>Call</BuildSelector>
+                  </section>
+              </div>
+          </aside>
+          <main className='p-4 sm:ml-64'>
+            <header className="mb-4">
+              <div className="header-inner flex justify-center flex-col">
+                  {build.cast === "" ? <span className="text-center text-white block py-5 text-2xl">No Boons Added</span> : null}
+              </div>
+            </header>          
+              {menuStatus && <FilterMenu />}
 
-            {weaponStatus && <Weapons 
-              name="Weapon"
-              data={weaponsData}
-              onItemClick={updateBuild}
-            />}
+              {activeIndex == 1 && <Weapons 
+                name="Weapon"
+                data={weaponsData}
+                onItemClick={updateBuild}
+              />}
 
-            {aspectStatus && <Weapons 
-              name="Aspect"
-              data={weaponsData[aspect].aspects}
-              onItemClick={updateAspect}
-            />}
+              {activeIndex == 2 && <Weapons 
+                name="Aspect"
+                data={weaponsData[aspect].aspects}
+                onItemClick={updateAspect}
+              />}
 
 
-           
-            <section className=''>
-                <h2>Boons</h2>
-                <div className="flex flex-row flex-wrap gap-4">
-                    <Card name="Test Card"/>
-                </div>
-            </section>
-        </main>
+            
+              <section className=''>
+                  <h2>Boons</h2>
+                  <div className='grid grid-cols-3 gap-4'>
+                    {activeIndex == 3 && attack.map((boon) => (
+                        <Card {...boon} key={boon.ID}
+                        />
+                    ))}
+
+                    {activeIndex == 4 && special.map((boon) => (
+                        <Card {...boon} key={boon.ID}
+                        />
+                    ))}
+
+
+                    {activeIndex == 5 && cast.map((boon) => (
+                        <Card {...boon} key={boon.ID}
+                        />
+                    ))}                  
+                  </div>
+              </section>
+          </main>
+        </div>
       </div>
     </>
   )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,23 @@
-import { ReactNode } from 'react'
-import Aphrodites_Aid from '../assets/img/_boons/Aphrodites_Aid.webp';
-
 
 interface Props {
-    children?: ReactNode;
-    name : string;
-
+    key: number;
+    name: string;
+    description: string;
+    img : string;
+    god: string;
 }
 
-const Card = ({children, name = "Boon Name"} : Props) => {
+const Card = ({key = 0, name = "Boon Name", description = "No Description Available",  img, god} : Props) => {
   return (
-    <div className='boon-card block max-w-sm p-6 border flex row border-gray-200 rounded-lg shadow'>
+    <div className='boon-card block max-w-sm p-6 border flex row border-gray-200 rounded-lg shadow' key={key} data-god={god}>
         <div className='card-head w-1/3'>
             <div className='gods'>
-                <img src={Aphrodites_Aid}/>
+                <img src={img}/>
             </div>
          </div>
         <div className='card-main w-2/3'>
             <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h3>
-            <p className="font-normal text-gray-700 dark:text-gray-400">{children}</p>
+            <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
         </div>
     </div>
   )
