607425d7d45b4faa9b09a44c343b9bb935102258
Collin Berg <berg.collin@gmail.com>
2025-01-09 11:37:38 -0500

    Updated Boons and gitignore

--- src/data/Boons.tsx
+++ src/data/Boons.tsx
@@ -8,5 +8,36 @@ export interface Boon {
 }
 
 export const boons : Boon[] = [
-
+    {
+        ID: 0,        
+        name : "Heaven's Vengeance",
+        god : "Zeus",
+        description : "After you take damage, your foe is struck by lightning.",
+        img : "https://static.wikia.nocookie.net/hades_gamepedia_en/images/6/6b/Zeus_Boon_Icon.png",
+        prerequisites: [],
+    },
+    {
+        name : "Lightning Reflexes",
+        god : "Zeus",
+        description : "After you Dash just before getting hit, a bolt strikes a nearby foes.",
+        img : "https://static.wikia.nocookie.net/hades_gamepedia_en/images/6/6b/Zeus_Boon_Icon.png",
+        ID: 1,
+        prerequisites: [],
+    },
+    {
+        name : "Storm Lightning",
+        god : "Zeus",
+        description : "Your chain-lightning effects bounce more times before expiring.",
+        img : "https://static.wikia.nocookie.net/hades_gamepedia_en/images/6/6b/Zeus_Boon_Icon.png",
+        ID: 2,
+        prerequisites: ['Lightning Strike', 'Electric Shot'],
+    },    
+    {
+        name : "High Voltage",
+        god : "Zeus",
+        description : "Your lightning bolt effects deal damage in a larger area.",
+        img : "https://static.wikia.nocookie.net/hades_gamepedia_en/images/6/6b/Zeus_Boon_Icon.png",
+        ID: 3,
+        prerequisites: ['Thunder Flourish', 'Thunder Dash','Zeus\' Aid'],
+    },    
 ];
