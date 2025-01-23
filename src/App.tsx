736977179bfc89732886fef2556d9607dc65ed4b
Collin Berg <berg.collin@gmail.com>
2025-01-23 13:10:40 -0500

    Added FIlter Function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 import BuildSelector from "./components/BuildSelector";
 import AppNav from "./components/AppNav";
@@ -13,34 +17,34 @@ import {special} from "./data/BoonsSpecial";
 import {cast} from "./data/BoonsCast"
 import { dash } from "./data/BoonsDash";
 import { call } from "./data/BoonsCall";
-import { boons } from "./data/Boons";
 
 import './App.css'
 
-interface Weapon {
+export interface Weapon {
   type: string;
   id: string;
   name: string;
   img: string;
 }
 
-interface Aspect {
+export interface Aspect {
   number: number;
   id: string;
   name: string;
   img: string;
 }
 
-interface Boon {
+export interface Boon {
   id: string;
   name: string;
-  god: string;
+  god: string;   
   description: string;
   img: string;
+  prerequisites ?: string[];
 }
 
-interface Build {
-  weapon: Weapon;
+export interface Build {
+  weapon: string;
   aspect: Aspect;
   attack: Boon;
   special: Boon;
@@ -51,12 +55,7 @@ interface Build {
 }
 
 const initialBuildState: Build = {
-  weapon: {
-    type: "",
-    id: "",
-    name: "",
-    img: "",
-  },
+  weapon: "",
   aspect: {
     number: 0,
     id: "",
@@ -104,28 +103,31 @@ const initialBuildState: Build = {
 function App() {
 
   const [build, setBuild] = useState<Build>(initialBuildState);
-  const [menuStatus, setMenuStatus] = useState(false);
+  const [boonList, setBoonList] = useState<Boon[]>([]);
+  const [selectedGod, setSelectedGod] = useState<string | null>(null);
   const [activeIndex, setActiveIndex] = useState(0);
   const [aspect, setAspect] = useState(0);
+
+  const handleFilterClick = (god: string) => {
+    selectedGod === god ? setSelectedGod(null) : setSelectedGod(god);
+  };
+
+  const filteredBoons = selectedGod
+  ? boonList.filter(boon => boon.god === selectedGod)
+  : boonList;
 
   const updateBuild = (item:number) => {
     setActiveIndex(2),
     setAspect(item),
     setBuild( build => (
       {...build,
-        weapon: {
-          type: weaponsData[item].type,
-          id: weaponsData[item].id,
-          name: weaponsData[item].name,
-          img: weaponsData[item].img,
-        }
+        weapon: weaponsData[item].id,
       }
     ));
     
   }
 
   const updateAspect = (item:number) => {
-    setMenuStatus(true)
     setActiveIndex(0),
     setBuild( build => (
       {...build,
@@ -166,46 +168,64 @@ function App() {
   }
   //This function accepts an array of Id's. Only one Id needs to be found for it to return as false.
   const isCardDisabled = (prerequisite: string[]) => {
-    //If there are no prerequisites, return false
+    //If there are no prerequisites, return false.
+    //Cards should be active by default
     if (prerequisite?.length == 0 ) return false;
     //If there are prerequisites, check if any of them are in the build.boons array
+    //If any of them are found, return false. because returning true would disable the card.
     return !prerequisite.includes(build.aspect.id);
   };
+
+
+  useEffect(function () {
+    async function fetchData() {
+      try{
+        const res = await fetch("http://localhost:9000/boons");
+        const boonList = await res.json();
+        console.log(boonList);
+        setBoonList(boonList);
+      } catch (error) {
+        console.log(error);
+      }
+      }
+    fetchData();
+  }, []);
 
   return (
     <>
       <div className="site-wrap">
-        <AppNav onResetClick={() => setBuild(initialBuildState) } />
+        <AppNav onResetClick={() => {
+            setBuild(initialBuildState)
+            setSelectedGod(null)
+        }
+         } />
         <div className="flex w-full pt-16 overflow-hidden" aria-hidden="true">
-          <aside id="default-sidebar" className="fixed pt-12 top-0 left-0 z-20 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="SIdebar">
-              <div className="h-full px-4 py-8 overflow-y-auto">
-                  <section id='weapon-side' className="space-y-2 font-medium">
-                      <h2>Weapon</h2>
-                      <div className='weapon_wrap'>
-                          <BuildSelector onClick={() => setActiveIndex(1)} attribute={build.weapon}>Weapon</BuildSelector>
-                      </div>
-                      <div className="weapon_wrap">
-                          <BuildSelector onClick={() => setActiveIndex(2)} attribute={build.aspect}>Aspect</BuildSelector>
-                      </div>
-                  </section>
-                  <section id='aspect-sIde'>
-                      <h2>Abilities</h2>
-                      <BuildSelector onClick={() => setActiveIndex(3)} attribute={build.attack}>Attack</BuildSelector>
-                      <BuildSelector onClick={() => setActiveIndex(4)} attribute={build.special}>Special</BuildSelector>
-                      <BuildSelector onClick={() => setActiveIndex(5)} attribute={build.cast}>Cast</BuildSelector>
-                      <BuildSelector onClick={() => setActiveIndex(6)} attribute={build.dash}>Dash</BuildSelector>
-                      <BuildSelector onClick={() => setActiveIndex(7)} attribute={build.call}>Call</BuildSelector>
-
-                  </section>
-              </div>
-          </aside>
-          <main className='p-4 sm:ml-64'>
+          <Sidebar>
+            <section id='weapon-side' className="space-y-2 font-medium">
+                  <h2>Weapon</h2>
+                  <div className='weapon_wrap'>
+                      <BuildSelector onClick={() => setActiveIndex(1)} attribute={build.weapon}>Weapon</BuildSelector>
+                  </div>
+                  <div className="weapon_wrap">
+                      <BuildSelector onClick={() => setActiveIndex(2)} attribute={build.aspect}>Aspect</BuildSelector>
+                  </div>
+              </section>
+              <section id='aspect-side'>
+                  <h2>Abilities</h2>
+                  <BuildSelector onClick={() => setActiveIndex(3)} attribute={build.attack}>Attack</BuildSelector>
+                  <BuildSelector onClick={() => setActiveIndex(4)} attribute={build.special}>Special</BuildSelector>
+                  <BuildSelector onClick={() => setActiveIndex(5)} attribute={build.cast}>Cast</BuildSelector>
+                  <BuildSelector onClick={() => setActiveIndex(6)} attribute={build.dash}>Dash</BuildSelector>
+                  <BuildSelector onClick={() => setActiveIndex(7)} attribute={build.call}>Call</BuildSelector>
+              </section>
+          </Sidebar>
+          <Main>
             <header className="mb-4">
               <div className="header-inner flex justify-center flex-col">
-                  {build.boons.length === 0 ? <span className="text-center text-white block py-5 text-2xl">No Boons Added</span> : <ul> {build.boons.map((boon) => (<Boon {...boon}  key={boon.id}  /> )) } </ul>}                
+                  {build.boons.length === 0 ? <span className="text-center text-white block py-5 text-2xl">No Boons Added</span> : <ul className="boon-list"> {build.boons.map((boon) => (<Boon {...boon}  key={boon.id} /> )) } </ul>}                
               </div>
             </header>          
-              {menuStatus && <FilterMenu />}
+              <FilterMenu onFilterClick={handleFilterClick} activeGod={selectedGod}/>
 
               {activeIndex == 1 && <Weapons 
                 name="Weapon"
@@ -258,17 +278,19 @@ function App() {
                     
                     ))}
 
+                    {filteredBoons.map((boon) => (
+                      //If the build.boons array does not contain the current boon, display the card.
+                      !build.boons.some(b => b.id === boon.id) && (
 
-                    {boons.map((boon) => (
-                        <Card {...boon}  key={boon.id} onClick={() => updateBoons(boon)}
-                        disabled={isCardDisabled(boon.prerequisites)}
-                        />
-                    
+                      <Card {...boon} key={boon.id} onClick={() => updateBoons(boon)}
+                      disabled={isCardDisabled(boon.prerequisites || [])}
+                      />
+                      )
                     ))}
 
                   </div>
               </section>
-          </main>
+          </Main>
         </div>
       </div>
     </>
--- src/components/FilterMenu.tsx
+++ src/components/FilterMenu.tsx
@@ -9,18 +9,45 @@ import Dionysus_symbol from '../assets/img/symbols/Dionysus_symbol.webp';
 import Aphrodite_symbol from '../assets/img/symbols/Aphrodite_symbol.webp';
 
 
-const FilterMenu = () => {
+const symbols = [
+  { god: 'Zeus', img: Zeus_symbol },
+  { god: 'Poseidon', img: Poseidon_symbol },
+  { god: 'Athena', img: Athena_symbol },
+  { god: 'Ares', img: Ares },
+  { god: 'Artemis', img: Artemis_symbol },
+  { god: 'Demeter', img: Demeter_symbol },
+  { god: 'Dionysus', img: Dionysus_symbol },
+  { god: 'Aphrodite', img: Aphrodite_symbol },
+];
+
+interface Props {
+  onFilterClick : (god : string) => void;
+  activeGod : string | null;
+}
+
+const FilterMenu = ({onFilterClick, activeGod} : Props) => {
+
+
+
   return (
     <section id='main-menu'>
         <ul className='nav type-nav'>
-            <li><img src={Zeus_symbol}/></li>
-            <li><img src={Poseidon_symbol} /></li>
-            <li><img src={Athena_symbol} /></li>
-            <li><img src={Ares}/></li>
-            <li><img src={Artemis_symbol}/></li>
-            <li><img src={Demeter_symbol} /></li>
-            <li><img src={Dionysus_symbol} /></li>
-            <li><img src={Aphrodite_symbol}/></li>
+
+        {symbols.map((symbol, index) => (
+          <button 
+            key={index}
+            value={symbol.god}
+            onClick={() => onFilterClick(symbol.god)}
+            className={`nav-item ${
+              symbol.god === activeGod 
+                ? 'active'
+                : ''
+            }`}
+          >
+            <img src={symbol.img} alt={`${symbol.god} Symbol`} />
+          </button>
+        ))}
+
         </ul>
     </section>
   )
